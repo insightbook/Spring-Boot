2178a7367b39ece78faf8276a03aa787ba940584
senseinsight <sense@insightbook.co.kr>
2019-03-13 09:26:31 +0900

    Add files via upload

---
+++ 02장/src/main/java/sample/controller/HelloController.java
@@ -0,0 +1,13 @@
+package sample.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+}
